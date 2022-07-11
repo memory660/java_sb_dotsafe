e4710782e4f294d2cb94a440571db8fdcafc31fe
duke <webcreation33@yahoo.fr>
2022-07-11 09:25:30 +0200

    AV55

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
 INSERT INTO roles(id, name) VALUES(1, 'ROLE_USER');
 INSERT INTO roles(id, name) VALUES(2, 'ROLE_MODERATOR');
 INSERT INTO roles(id, name) VALUES(3, 'ROLE_ADMIN');
+
+
+INSERT INTO techno(id, name) VALUES(1, 'Techno1');
+INSERT INTO techno(id, name) VALUES(2, 'Techno2');
+INSERT INTO techno(id, name) VALUES(3, 'Techno3');
+
+INSERT INTO project(id, name) VALUES(1, 'Project1');
+INSERT INTO project(id, name) VALUES(2, 'Project2');
+INSERT INTO project(id, name) VALUES(3, 'Project3');
